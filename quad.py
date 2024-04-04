3f06e433d7d6a3dd5d681846d727292786f0b827
w4lyf <159780645+w4lyf@users.noreply.github.com>
2024-04-05 01:01:59 +0530

    Update quad.py

--- quad.py
+++ quad.py
@@ -1,4 +1,4 @@
-import cmath
+import math
 
 a = float(input("Enter the coefficient of x^2 (a): "))
 b = float(input("Enter the coefficient of x (b): "))
@@ -6,7 +6,15 @@ c = float(input("Enter the constant term (c): "))
 
 d = (b**2) - (4*a*c)
 
-sol1 = (-b - cmath.sqrt(d)) / (2*a)
-sol2 = (-b + cmath.sqrt(d)) / (2*a)
-
-print("The solutions are: {0} and {1}".format(sol1, sol2))
+if d >= 0:
+    
+    sqrt_d = math.sqrt(d)
+    sol1 = (-b - sqrt_d) / (2*a)
+    sol2 = (-b + sqrt_d) / (2*a)
+    print("The real roots are: {0} and {1}".format(sol1, sol2))
+else:
+    
+    sqrt_d = math.sqrt(abs(d))
+    real_part = -b / (2*a)
+    imag_part = sqrt_d / (2*a)
+    print("The complex roots are: {0} + {1}i and {0} - {1}i".format(real_part, imag_part))
